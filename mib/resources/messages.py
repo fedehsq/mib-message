7ac30cff7e3af4e1beaf1ef29ffa28fd1fa67bb6
fedehsq <fedebernacca@gmail.com>
2021-12-02 14:21:41 +0100

    bug fixes

--- mib/resources/messages.py
+++ mib/resources/messages.py
@@ -1,6 +1,7 @@
 from re import M
 from flask import request, jsonify
 from sqlalchemy.sql.expression import or_
+from sqlalchemy.sql.functions import user
 from mib.dao.message_manager import MessageManager
 from mib.models.message import Message
 from mib import db
@@ -48,20 +49,17 @@ def get_message_by_id(message_id):
     }
     return jsonify(response_object), 200
 
-def create_message(user_email):
+def create_message():
     """
     This method allows the creation of a new message
     for the user with email == user_email.
     """
-    print('prima di create')
     post_data = request.get_json()
-    print('prima di get json')
-    print(post_data)
-    #mail_sender = post_data.get('sender')
-    email_receiver = post_data.get('receiver')
     message = Message()
-    message.set_sender(user_email)
-    message.set_receiver(email_receiver)
+    message.set_sender_id(post_data.get('sender_id'))
+    message.set_sender(post_data.get('sender'))
+    message.set_receiver_id(post_data.get('receiver_id'))
+    message.set_receiver(post_data.get('receiver'))
     message.set_body(post_data.get('body'))
     message.set_photo(post_data.get('photo'))
     message.set_timestamp(post_data.get('timestamp'))
@@ -70,11 +68,6 @@ def create_message(user_email):
     message.set_bold(post_data.get('bold'))
     message.set_italic(post_data.get('italic'))
     message.set_underline(post_data.get('underline'))
-    print('messaggio!')
-    print(message.serialize())
-    #just for test purposes set sent=1
-    #message.set_sent(1)
-    ###################
     MessageManager.create_message(message)
     response_object = {
         'status': 'success',
@@ -83,23 +76,23 @@ def create_message(user_email):
     }
     return jsonify(response_object), 201
 
-def update_message(user_email):
+def update_message(message_id):
     """
     This method allows the edit of an existing message 
-    for the user with email == user_email.
+    for the user with id == user_id.
     """
     post_data = request.get_json()
-    #mail_sender = post_data.get('sender')
-    email_receiver = post_data.get('receiver')
-    message = MessageManager.retrieve_by_id(post_data.get('id'))
+    message = MessageManager.retrieve_by_id(message_id)
     if message is None:
         response_object = {
             'status': 'failure',
             'message': 'Message not found',
         }
         return jsonify(response_object), 404
-    message.set_sender(user_email)
-    message.set_receiver(email_receiver)
+    message.set_sender_id(post_data.get('sender_id'))
+    message.set_sender(post_data.get('sender'))
+    message.set_receiver_id(post_data.get('receiver_id'))
+    message.set_receiver(post_data.get('receiver'))
     message.set_body(post_data.get('body'))
     message.set_photo(post_data.get('photo'))
     message.set_timestamp(post_data.get('timestamp'))
@@ -110,9 +103,6 @@ def update_message(user_email):
     message.set_bold(post_data.get('bold'))
     message.set_italic(post_data.get('italic'))
     message.set_underline(post_data.get('underline'))
-    #just for test purposes set sent=1
-    #message.set_sent(1)
-    ###################
     MessageManager.update_message(message)
     response_object = {
         'status': 'success',
@@ -121,18 +111,18 @@ def update_message(user_email):
     }
     return jsonify(response_object), 200
 
-
-def search_message(user_email):
+def search_message():
     # gets from the request the id of the user and the desidered list
     post_data = request.get_json()
-    #id_user = post_data.get('id')
+    user_id = post_data.get('user_id')
+    user_email = post_data.get('user_email')
     body = post_data.get('body')
     email = post_data.get('sender')
     date = post_data.get('date')
 
     # search
     filtered_inbox, filtered_sent, filtered_scheduled = \
-        search_messages(user_email, body, email, date)
+        search_messages(user_id, user_email, body, email, date)
 
     response_object = {
         'status': 'success',
@@ -145,14 +135,18 @@ def search_message(user_email):
     }
     return jsonify(response_object), 200
 
-def get_inbox_messages(user_email):
+def get_inbox_messages():
     """    
     this function returns the inbox messages
-    for the user with email == user_email.
+    for a given user.
     """
+    post_data = request.get_json()
+    user_id = post_data.get('user_id')
+    user_email = post_data.get('user_email')
     messages = db.session.query(Message).filter(\
         # in this way the messages read don't deseappear in the mailbox
-        Message.receiver == user_email, 
+        Message.receiver == user_email,
+        Message.receiver_id == user_id,
         or_(Message.sent == 1,  Message.sent == 2))
     response_object = {
         'status':'success',
@@ -161,13 +155,17 @@ def get_inbox_messages(user_email):
     }
     return jsonify(response_object), 200
 
-def get_scheduled_messages(user_email):
+def get_scheduled_messages():
     """    
     this function returns the scheduled messages
     for the user with email == user_email.
     """
+    post_data = request.get_json()
+    user_id = post_data.get('user_id')
+    user_email = post_data.get('user_email')
     messages = db.session.query(Message).filter(\
         Message.sender == user_email,
+        Message.sender_id == user_id,
         Message.scheduled == True).all() 
     response_object = {
         'status':'success',
@@ -177,13 +175,17 @@ def get_scheduled_messages(user_email):
     return jsonify(response_object), 200
 
 
-def get_sent_messages(user_email):
+def get_sent_messages():
     """    
     this function returns the sent messages
     for the user with email == user_email.
     """
+    post_data = request.get_json()
+    user_id = post_data.get('user_id')
+    user_email = post_data.get('user_email')
     messages = db.session.query(Message).filter(\
-        Message.sender == user_email, 
+        Message.sender == user_email,
+        Message.sender_id == user_id,
         # in this way the messages read don't deseappear in the mailbox
         or_(Message.sent == 1, Message.sent == 2),
         )
@@ -194,13 +196,17 @@ def get_sent_messages(user_email):
     }
     return jsonify(response_object), 200
 
-def get_draft_messages(user_email):
+def get_draft_messages():
     """    
     this function returns the draft messages
     for the user with email == user_email.
     """
+    post_data = request.get_json()
+    user_id = post_data.get('user_id')
+    user_email = post_data.get('user_email')
     messages = db.session.query(Message).filter(\
         Message.sender == user_email,
+        Message.sender_id == user_id,
         Message.draft == True)
     response_object = {
         'status':'success',
@@ -210,21 +216,27 @@ def get_draft_messages(user_email):
     return jsonify(response_object), 200
 
 
-def get_notifications(user_email):
+def get_notifications():
     """
     This function returns the notification numbers
     for the user with email == user_email.
     """
-
+    post_data = request.get_json()
+    user_id = post_data.get('user_id')
+    user_email = post_data.get('user_email')
+    print(user_id)
+    print(user_email)
     inbox = db.session.query(Message).filter(\
         Message.receiver == user_email,
+        Message.receiver_id == user_id,
         Message.sent == 1, 
         Message.read == 0).count()
     sent = db.session.query(Message).filter(\
         Message.sender == user_email,
+        Message.sender_id == user_id,
         Message.sent == 1, 
-        Message.read == 1).count()
-       
+        or_(Message.read == 1, Message.read == 2)).count()
+    
     response_object = {
         'status':'success',
         'message':'Successfully delivered notifications',
@@ -239,114 +251,34 @@ def get_notifications(user_email):
 ###############-------Auxiliary Functions-------##################
 ##################################################################
 
-def search_messages(user_email, body, sender, date):
+def search_messages(user_id, user_email, body, sender, date):
     """
     Given the body (and/or) the sender (and/or) the date, 
     returns the filtered messages
     """
-    """ if not body and not sender and not date:
-        return 0, 0, 0
-    try:
-        msg_date = datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%Y')
-    except:
-        msg_date = ''"""
-
     if date:
         #  2022-06-05 12:12:00
         date = datetime.strptime(date, '%Y-%m-%d')
     inbox = db.session.query(Message).filter(\
+        Message.receiver_id == user_id, 
         Message.receiver == user_email, 
         Message.sent != 0,
         Message.body.contains(body),
         Message.timestamp.cast(Date) == date if date else True
         ).all()
     sent = db.session.query(Message).filter(\
+        Message.sender_id == user_id, 
         Message.sender == user_email, 
         Message.sent != 0,
         Message.body.contains(body),
         Message.timestamp.cast(Date) == date if date else True
         ).all()
     scheduled = db.session.query(Message).filter(\
+        Message.sender_id == user_id, 
         Message.sender == user_email, 
         Message.scheduled == True,
         Message.body.contains(body),
         Message.sender.contains(sender),
         Message.timestamp.cast(Date) == date if date else True     
         ).all()
-    print('schedulati!!!!!!')
-    for s in scheduled:
-        print(s.serialize())
     return inbox, sent, scheduled
-
-"""def filter_list(messages, body, sender, date):
-    Returns a filtered list 
-    for m in messages:
-        if body in m.body and sender in m.sender and date in m.timestamp:
-            messages.append(m)"""
-
-
-"""# Given the body, the sender and the date, returns the filtered messages
-def search_messages(id,msg_field, msg_user, msg_date):
-    res_to_be_sent=[]
-    res_received=[]
-    res_sent=[]
-    
-    scheduled = db.session.query(Message).filter(Message.sender==id, Message.scheduled==True)
-    inbox = db.session.query(Message).filter(Message.receiver==id, Message.sent==True)
-    sent = db.session.query(Message).filter(Message.sender==id, Message.sent==True)    
-    
-    if msg_date is None:
-        msg_date=""
-    if msg_field is None:
-        msg_field=""
-    if msg_user is None:
-        msg_user=""
-
-    #assembling the lists
-    to_be_sent_msgs=[]
-    for m in scheduled:
-        to_be_sent_msgs.append(m.to_string())
-    sent_msgs=[]
-    for m in sent:
-        sent_msgs.append(m.to_string())
-    received_msgs=[]
-    for m in inbox:
-        received_msgs.append(m.to_string())
-
-    #checks for three none args
-    if not msg_field and not msg_user and not msg_date:
-        return 0,0,0
-
-    #checks for date format 
-        if msg_date:
-        try:
-            msg_date=datetime.strptime(msg_date, '%Y-%m-%d').strftime('%d/%m/%Y')
-            print(msg_date)
-        except:
-            return "error","error","error" 
-
-    for cm in to_be_sent_msgs:
-        curr=json.loads(cm)
-        body=curr["body"]
-        timestamp=curr["timestamp"]
-        if msg_field in body and msg_date in timestamp:
-            if msg_field or msg_user or msg_date: 
-                res_to_be_sent.append(json.dumps(cm))
-
-    for cm in sent_msgs:
-        curr=json.loads(cm)
-        body=curr["body"]
-        timestamp=curr["timestamp"]
-        if msg_field in body and msg_date in timestamp:
-            res_sent.append(json.dumps(cm))
-    
-    for cm in received_messages:
-        curr=json.loads(cm)
-        body=curr["body"]
-        sender=curr["sender"]
-        timestamp=curr["timestamp"]
-        if msg_field in body and msg_user in sender and msg_date in timestamp:
-            res_received.append(json.dumps(cm))
-    
-    return res_received,res_sent,res_to_be_sent
-    """
